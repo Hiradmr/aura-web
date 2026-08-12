ee501f254ca12ef44d04febaab83a276fc6f759d
Hiradmr <hiramervedemir@gmail.com>
2026-08-13 02:30:49 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { site } from "@/lib/config";
+import { site } from "../lib/config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
